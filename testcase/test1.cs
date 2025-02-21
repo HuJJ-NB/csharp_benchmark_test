c2a7923e8735b4c76aa71480a086156645a32889
胡家骏 <91231458+HuJJ-NB@users.noreply.github.com>
2025-02-21 18:38:10 +0800

    Create test1.cs

---
+++ testcase/test1.cs
@@ -0,0 +1,14 @@
+namespace TestForAddMul;
+
+public class AddMul
+{
+    public static int Mul(int a, int b)
+    {
+        return a * b;
+    }
+
+    public static int Add(int a, int b)
+    {
+        return a + b;
+    }
+}
