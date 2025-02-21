3d1d3b07911fbf0dadfa0d03f3070604580da6a4
胡家骏 <91231458+HuJJ-NB@users.noreply.github.com>
2025-02-21 18:51:25 +0800

    Update test1.cs

--- testcase/test1.cs
+++ testcase/test1.cs
@@ -1,14 +1,16 @@
-namespace TestForAddMul;
+namespace TestForAddMul {
 
-public class AddMul
-{
-    public static int Mul(int a, int b)
+    public class AddMul
     {
-        return a * b;
+        public static int Mul(int a, int b)
+        {
+            return a * b;
+        }
+    
+        public static int Add(int a, int b)
+        {
+            return a + b;
+        }
     }
-
-    public static int Add(int a, int b)
-    {
-        return a + b;
-    }
+    
 }
