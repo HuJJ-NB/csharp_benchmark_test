b48fa14c30530365d56689325754c35c6198cb42
胡家骏 <91231458+HuJJ-NB@users.noreply.github.com>
2025-02-22 12:47:16 +0800

    Update and rename test1.cs to AddMulTest.cs

---
+++ testcase/AddMulTest.cs
@@ -0,0 +1,12 @@
+public class AddMulTest
+{
+    public static int Mul(int a, int b)
+    {
+        return a * b;
+    }
+
+    public static int Add(int a, int b)
+    {
+        return a + b;
+    }
+}
